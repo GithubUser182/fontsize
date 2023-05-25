01f165934ab85bf2fe67b93d77fa626207d13a62
GithubUser182 <112442002+GithubUser182@users.noreply.github.com>
2023-05-25 16:26:00 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
+noseX=0
+noseY=0
+leftWristX=0
+rightWristX=0
+difference=0
+
 function setup(){
     video=createCapture(VIDEO)
-    video.size(1300,500)
+    video.size(700,500)
 
-    canvas=createCanvas(550,550)
-    canvas.position(1000,150)
+    canvas=createCanvas(1200,600)
+    canvas.position(700,150)
     poseNet=ml5.poseNet(video, modelLoaded )
     poseNet.on('pose', gotPoses)
 }
 
 function draw(){
     background("#8dc995")
+    document.getElementById("font_size").innerHTML="Width and Height ="+difference+"px."
+    fill("#e31489")
+    stroke("#e31489")
+    text('LaL',noseX,noseY)
+    textSize(difference)
 }
 
 function modelLoaded(){
@@ -19,5 +30,13 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results)
+        noseX=results[0].pose.nose.x
+        noseY=results[0].pose.nose.y
+        console.log("noseX = "+noseX + " | noseY = "+noseY)
+
+        leftWristX=results[0].pose.leftWrist.x
+        rightWristX=results[0].pose.rightWrist.x
+        difference=floor(leftWristX-rightWristX)
+        console.log("leftWristX = "+leftWristX + " | rightWristX = "+rightWristX + " | Difference = "+difference)
     }
 }
